7d75f33910aab3a381362bcb3476d4a47fbed993
phuoc <thorbmt@gmail.com>
2019-12-22 12:05:46 +0700

    renew

--- app/src/main/java/app/creativestudio/dtu/truyenlalala/Ref/AppConfig.java
+++ app/src/main/java/app/creativestudio/dtu/truyenlalala/Ref/AppConfig.java
@@ -1,7 +1,7 @@
 package app.creativestudio.dtu.truyenlalala.Ref;
 
 public class AppConfig {
-    public static final String SERVER = "http://192.168.43.235:80";
+    public static final String SERVER = "http://192.168.1.179:8000";
     public static final String DEFAULT_PHOTO_URL = "https://is5-ssl.mzstatic.com/image/thumb/Purple4/v4/1f/17/82/1f1782c9-4d2b-3c3e-2e2f-ca828f25f5d2/source/512x512bb.jpg";
     public static final String APP_URL ="";
     public static final String WEB_URL="";
